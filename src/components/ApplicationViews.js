abc9eea50c2432dc68750f21a83daa987efff0b0
Jayna Leitze <jayna.leitze@gmail.com>
2021-02-15 10:50:01 -0600

    gamer can register new game

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,34 @@
-import React from "react"
-import { Route } from "react-router-dom"
+import React from "react";
+import { Route } from "react-router-dom";
+import { GameList } from "./game/GameList.js";
+import { GameProvider } from "./game/GameProvider.js";
+import { EventList } from "./event/EventList.js";
+import { EventProvider } from "./event/EventProvider.js";
+import { GameForm } from "./game/GameForm.js";
 
 export const ApplicationViews = () => {
-    return <>
-        <main style={{
-            margin: "5rem 2rem",
-            backgroundColor: "lightgoldenrodyellow"
-        }}>
-            Application views
-        </main>
+  return (
+    <>
+      <main
+        style={{
+          margin: "5rem 2rem",
+          lineHeight: "1.75rem",
+        }}
+      >
+        <GameProvider>
+          <Route exact path="/">
+            <GameList />
+          </Route>
+          <Route exact path="/games/new">
+            <GameForm />
+          </Route>
+        </GameProvider>
+        <EventProvider>
+          <Route exact path="/events">
+            <EventList />
+          </Route>
+        </EventProvider>
+      </main>
     </>
-}
+  );
+};
---
+++ src/components/game/GameList.js
@@ -0,0 +1,38 @@
+import React, { useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { GameContext } from "./GameProvider.js";
+
+export const GameList = (props) => {
+  const history = useHistory();
+  const { games, getGames } = useContext(GameContext);
+
+  useEffect(() => {
+    getGames();
+  }, []);
+
+  return (
+    <article className="games">
+      <button
+        className="btn btn-2 btn-sep icon-create"
+        onClick={() => {
+          history.push({ pathname: "/games/new" });
+        }}
+      >
+        Register New Game
+      </button>
+      {games.map((game) => {
+        return (
+          <section key={`game--${game.id}`} className="game">
+            <div className="game__title">{game.title}</div>
+            <div className="game__players">
+              {game.number_of_players} players needed
+            </div>
+            <div className="game__description">
+              Description:{game.description}
+            </div>
+          </section>
+        );
+      })}
+    </article>
+  );
+};
